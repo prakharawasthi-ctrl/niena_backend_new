7a3b573c28a2dabddb478ff0f0818a23d04cc8c8
Prakhar Awasthi <64955939+prakharawasthi-ctrl@users.noreply.github.com>
2025-01-03 17:39:03 +0530

    Update server.js

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.use(express.json());
 
 // CORS configuration
 const corsOptions = {
-  origin: "http://niena-backend-new-adov.vercel.app", // Allow requests from your frontend
+  origin: "https://niena-backend-new-adov-nkakcy0go.vercel.app", // Allow requests from your frontend
   methods: "GET,POST,PUT,DELETE", // Allow these methods
   allowedHeaders: "Content-Type,Authorization", // Allow these headers
 };
